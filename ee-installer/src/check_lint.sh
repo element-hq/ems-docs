4866af5ae344324c991dd3cca9920a823d542eec
Karl Abbott <karl1@element.local>
2022-02-16 09:14:24 -0500

    Moving scripts over to tools and updating them to run from the tools directory when called from anywhere.

--- ee-installer/src/check_lint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-for i in $(ls *.md); do ./ensure_80char.sh "$i" && ./fix_code_fence.sh "$i" && ./fix_unordered_list.sh "$i" && markdownlint -c "../../lint/config/default.yml" -c "../../lint/config/summary.yml" -f "$i"; done
